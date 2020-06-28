75d4356814c1434115f7ad7463ec1f3652b4d1d8
dongja3 <jackie.dong@oocl.com>
2020-06-28 15:42:35 +0800

    add activeMQConfig

---
+++ src/main/java/com/oocl/workshop/intern/infrastructure/common/event/DemoConsumer.java
@@ -0,0 +1,27 @@
+package com.oocl.workshop.intern.infrastructure.common.event;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+
+import javax.jms.Session;
+
+import static com.oocl.workshop.intern.infrastructure.ActiveMQConfig.INTERN_QUEUE;
+
+public class DemoConsumer {
+    @JmsListener(destination = INTERN_QUEUE)
+    public void receiveMessage(@Payload DomainEvent event,
+                               @Headers MessageHeaders headers,
+                               Message message, Session session) {
+        System.out.println("received <" + event + ">");
+
+        System.out.println("- - - - - - - - - - - - - - - - - - - - - - - -");
+        System.out.println("######          Message Details           #####");
+        System.out.println("- - - - - - - - - - - - - - - - - - - - - - - -");
+        System.out.println("headers: " + headers);
+        System.out.println("message: " + message);
+        System.out.println("session: " + session);
+        System.out.println("- - - - - - - - - - - - - - - - - - - - - - - -");
+    }
+}
--- src/main/java/com/oocl/workshop/intern/infrastructure/common/event/EventPublisher.java
+++ src/main/java/com/oocl/workshop/intern/infrastructure/common/event/EventPublisher.java
@@ -1,12 +1,17 @@
 package com.oocl.workshop.intern.infrastructure.common.event;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
+
+import static com.oocl.workshop.intern.infrastructure.ActiveMQConfig.INTERN_QUEUE;
 
 @Service
 public class EventPublisher {
+    @Autowired
+    private JmsTemplate jmsTemplate;
 
     public void publish(DomainEvent event) {
-        //send to MQ
-        //mq.send(event);
+        jmsTemplate.convertAndSend(INTERN_QUEUE, event);
     }
 }
