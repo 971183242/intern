a1034ee00fedcedf2ec1a991124101b380d7a033
zhangco2 <colten.zhang@oocl.com>
2020-06-29 18:27:32 +0800

    update UserRepo

--- src/main/java/com/oocl/workshop/intern/domain/profile/repository/facade/UserRepo.java
+++ src/main/java/com/oocl/workshop/intern/domain/profile/repository/facade/UserRepo.java
@@ -2,10 +2,11 @@ package com.oocl.workshop.intern.domain.profile.repository.facade;
 
 import com.oocl.workshop.intern.domain.profile.entity.UserType;
 import com.oocl.workshop.intern.domain.profile.repository.po.UserPo;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
 import java.util.List;
 
 public interface UserRepo extends JpaRepository<UserPo, String> {
@@ -14,4 +15,8 @@ public interface UserRepo extends JpaRepository<UserPo, String> {
     List<UserPo> findByUserTypeAndRoleContains(UserType userType, String role);
 
     List<UserPo> findByUserTypeAndTeamId(UserType userType, String teamId);
+
+    @Query("select u from UserPo u where u.userType = 'INTERN' and u.teamId = :teamId and u.internPeriod.dateFrom <= :toDate " +
+            "and (u.internPeriod.dateTo >= :fromDate or u.internPeriod.dateTo is null)")
+    List<UserPo> findTeamActiveInterns(@Param("teamId") String teamId, @Param("fromDate") Date fromDate, @Param("toDate") Date toDate);
 }
