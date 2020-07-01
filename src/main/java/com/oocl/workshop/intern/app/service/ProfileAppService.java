aa27dd27f349ed0c486d8a8dea7abe30469d0bb1
guda <dave.gu@oocl.com>
2020-07-01 19:40:46 +0800

    add getInterns api

--- src/main/java/com/oocl/workshop/intern/app/service/ProfileAppService.java
+++ src/main/java/com/oocl/workshop/intern/app/service/ProfileAppService.java
@@ -1,4 +1,10 @@
 package com.oocl.workshop.intern.app.service;
 
+import com.oocl.workshop.intern.domain.profile.entity.Intern;
+
+import java.util.Date;
+import java.util.List;
+
 public interface ProfileAppService {
+    List<Intern> getInterns(String team, Date parseDate);
 }
