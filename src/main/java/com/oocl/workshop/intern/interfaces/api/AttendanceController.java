b2058b6a096def95c8f2f075f69a2252ede9fd24
guda <dave.gu@oocl.com>
2020-07-08 20:36:34 +0800

    add attendanceController

--- src/main/java/com/oocl/workshop/intern/interfaces/api/AttendanceController.java
+++ src/main/java/com/oocl/workshop/intern/interfaces/api/AttendanceController.java
@@ -1,9 +1,17 @@
 package com.oocl.workshop.intern.interfaces.api;
 
+import com.oocl.workshop.intern.interfaces.dto.attendance.AttendanceDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/attendance")
 public class AttendanceController {
+
+    @GetMapping("/period/{userId}/{date}")
+    public AttendanceDTO getPeriodAttendance(@PathVariable String userId, @PathVariable String date) {
+        return new AttendanceDTO();
+    }
 }
--- src/main/java/com/oocl/workshop/intern/interfaces/dto/attendance/AttendanceDTO.java
+++ src/main/java/com/oocl/workshop/intern/interfaces/dto/attendance/AttendanceDTO.java
@@ -1,4 +1,8 @@
 package com.oocl.workshop.intern.interfaces.dto.attendance;
 
+import lombok.Data;
+
+@Data
 public class AttendanceDTO {
+    private String userId;
 }
