706508f370089a139fcee765fdeeb094d3f12cfd
CUIST <stike.cui@oocl.com>
2020-07-14 17:31:33 +0800

    AttendanceControllerTest

--- src/test/java/com/oocl/workshop/intern/interfaces/api/AttendanceControllerTest.java
+++ src/test/java/com/oocl/workshop/intern/interfaces/api/AttendanceControllerTest.java
@@ -13,7 +13,10 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -28,21 +31,14 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class AttendanceControllerTest {
 
+    @Autowired
     private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     private AttendanceAppService attendanceAppService;
-
-    @InjectMocks
-    AttendanceController attendanceController;
-
-    @BeforeEach
-    void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(attendanceController).build();
-        MockitoAnnotations.initMocks(attendanceAppService);
-    }
 
     @Test
     void should_get_empty_attendance_data_from_attendance_controller() throws Exception {
