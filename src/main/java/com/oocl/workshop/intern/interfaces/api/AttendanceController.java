442d83efdf7271594a713c722c95610450e493fe
GengFo <gengfo@oocl.com>
2020-07-21 17:49:35 +0800

    [integration] update error

--- src/main/java/com/oocl/workshop/intern/interfaces/api/AttendanceController.java
+++ src/main/java/com/oocl/workshop/intern/interfaces/api/AttendanceController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import sun.java2d.cmm.Profile;
+
 
 import java.text.ParseException;
 import java.util.ArrayList;
