305855586cd21cd3382bf6bc52830429aedae8a4
zhangco2 <colten.zhang@oocl.com>
2020-07-21 17:58:56 +0800

    更新初始化数据

--- src/main/resources/sqls/prod-data.sql
+++ src/main/resources/sqls/prod-data.sql
@@ -3,7 +3,7 @@
 insert into T_SETTLEMENT_DAY(day) values(20);
 
 -- teams
-insert into T_TEAM(team_id,name, team_leader_id) values('TMS', 'tms', 'LUAL');
+insert into T_TEAM(team_id,name, team_leader_id) values('TMS', 'TMS', 'LUAL');
 insert into T_TEAM(team_id,name, team_leader_id) values('WMS/DCS', 'WMS/DCS','YUANFR');
 insert into T_TEAM(team_id,name, team_leader_id) values('IPS', 'IPS','ZHUJO3');
 insert into T_TEAM(team_id,name, team_leader_id) values('GPS', 'GPS', 'GEJO');
