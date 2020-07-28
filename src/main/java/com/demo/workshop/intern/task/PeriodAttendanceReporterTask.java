0f089f693790552e84e013e88d8785a94b8a735e
WANGWI4 <will.l.y.wang@oocl.com>
2020-07-28 18:18:21 +0800

    get Settlement day from dataBase

--- src/main/java/com/demo/workshop/intern/task/PeriodAttendanceReporterTask.java
+++ src/main/java/com/demo/workshop/intern/task/PeriodAttendanceReporterTask.java
@@ -1,10 +1,10 @@
 package com.demo.workshop.intern.task;
 
-import com.demo.workshop.intern.domain.report.repostitory.po.MonthlySettlementDayRule;
-import com.google.gson.Gson;
+import com.demo.workshop.intern.domain.report.repostitory.facade.MonthlySettlementDayRuleRepo;
 import com.demo.workshop.intern.support.ActiveMQConfig;
 import com.demo.workshop.intern.support.common.event.DomainEvent;
 import com.demo.workshop.intern.support.common.event.EventPublisher;
+import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,10 +24,13 @@ public class PeriodAttendanceReporterTask {
     @Autowired
     private EventPublisher eventPublisher;
 
+    @Autowired
+    private MonthlySettlementDayRuleRepo ruleRepo;
+
 //        @Scheduled(cron = "0/5 * * * * MON-FRI ")
     @Scheduled(cron = "0 45 8 * * MON-FRI ")
     public void execute() {
-        if (LocalDate.now().getDayOfMonth() - MonthlySettlementDayRule.DEFAULT_DAY > 3) {
+        if (LocalDate.now().getDayOfMonth() - ruleRepo.getMonthlySettlementDay() > 3) {
             DomainEvent event = new DomainEvent();
             event.setId(UUID.randomUUID().toString());
             event.setTimestamp(new Date());
